68ef82392fe502897598cfd71ee93df9fc7d32d7
Jack Joliet <jrjoliet@iu.edu>
2020-09-22 18:10:20 -0400

    initial static build files

---
+++ build/precache-manifest.5497ff20b1e9002effc2f5f9513c0808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423814796d6e0e65b7f01470c41cabc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af446ea14128bfccf72",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "e795af5b10a07e0b7944",
+    "url": "/static/js/2.6a7b70ee.chunk.js"
+  },
+  {
+    "revision": "6d6c0d1371e570614ef23e12cbfed171",
+    "url": "/static/js/2.6a7b70ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af446ea14128bfccf72",
+    "url": "/static/js/main.3d9c9b36.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
