783d8edebb43be0483bb089d8aa750e105a990f4
Jack Joliet <jrjoliet@iu.edu>
2020-10-05 09:42:56 -0400

    fix bcrypt issue

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const empty = require('is-empty');
 const { generateAuthToken, validateLogin } = require('../models/user');
 const constants = require('../utils/constants');
