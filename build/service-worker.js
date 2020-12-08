62fa2c3971da23e99c2581c080ac366584b1c66a
Jack Joliet <jrjoliet@iu.edu>
2020-12-08 13:10:24 -0500

    star change

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72ce413e4c74b357bb2edafde884dbce.js"
+  "/precache-manifest.b964d1e736286c38128f9d907518137f.js"
 );
 
 self.addEventListener('message', (event) => {
